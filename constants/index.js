b5701f46300fbd91bf99351938b632cb220473d9
czy0729 <402731062@qq.com>
2019-07-06 09:26:58 +0800

    no message

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2019-06-27 10:35:14
  * @Last Modified by: czy0729
- * @Last Modified time: 2019-07-05 22:03:11
+ * @Last Modified time: 2019-07-06 09:26:20
  */
 import getConfig from 'next-server/config'
 
@@ -12,7 +12,7 @@ const { publicRuntimeConfig } = getConfig()
 export const { linkPrefix } = publicRuntimeConfig
 
 export const URL_LOGIN =
-  'http://ms.litku.yzess.cn/login?forward=http://localhost:3000'
+  'http://ms.litku.yzess.cn/login?forward=https://czy0729.github.io/yun-sass-admin/out/'
 export const URL_API_HOST = 'http://api.sdb.yzess.cn'
 export const URL_UPLOAD_SIGN = 'http://api.sdb.yzess.cn/upload/sign'
 export const URL_UPLOAD_VERIFY = 'http://api.sdb.yzess.cn/upload/verify'
